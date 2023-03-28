67bbe7e494a1f76ba5655edbbe0919bd1c4e0737
aluminixum <aluminixum@gmail.com>
2023-03-28 15:20:14 +0900

    \277(0x7f) to \377(0xff)

--- shredder.sh
+++ shredder.sh
@@ -1,13 +1,25 @@
 #!/bin/bash
 
 # usage
-if [ -z "$1" ]; then echo -e "Usage\n./shredder.sh target"; exit 1; fi
+if [ -z "$1" ]
+    then echo -e "Usage\n./shredder.sh target"
+    exit 1
+fi
 
 # dd block size
 ddbs=512
 
 # target
 tgt="$1"
+
+# check file or block 0:block 1:file
+echo ""
+test -b "$tgt"
+if [ $? != 0 ]; then
+    tgttype=1
+else
+    tgttype=0
+fi
 
 # get file size (byte)
 echo "get size..."
@@ -26,29 +38,43 @@ ddopt="conv=notrunc,noerror,fdatasync"
 # delete cache
 echo "delete cache data..."
 dd of="$tgt" oflag=nocache conv=notrunc,fdatasync count=0 > /dev/null 2>&1
+sync
 
 # start dd shredder
 echo -e "\n---- start ----\n"
 echo "write:random"
 dd if=/dev/urandom of="$tgt" bs=$ddbs count=$tcount $ddopt > /dev/null 2>&1
+hexdump -C "$tgt"|head -n 20
 echo "write:0x00"
 dd if=/dev/zero of="$tgt" bs=$ddbs count=$tcount $ddopt > /dev/null 2>&1
+hexdump -C "$tgt"|head -n 20
 echo "write:0xBB"
 tr "\000" "\273" < /dev/zero|dd of="$tgt" bs=$ddbs count=$tcount $ddopt > /dev/null 2>&1
+hexdump -C "$tgt"|head -n 20
 echo "write:0x33"
 tr "\000" "\063" < /dev/zero|dd of="$tgt" bs=$ddbs count=$tcount $ddopt > /dev/null 2>&1
+hexdump -C "$tgt"|head -n 20
 echo "write:0xFF"
-tr "\000" "\277" < /dev/zero|dd of="$tgt" bs=$ddbs count=$tcount $ddopt > /dev/null 2>&1
+tr "\000" "\377" < /dev/zero|dd of="$tgt" bs=$ddbs count=$tcount $ddopt > /dev/null 2>&1
+hexdump -C "$tgt"|head -n 20
 echo "write:0x77"
 tr "\000" "\167" < /dev/zero|dd of="$tgt" bs=$ddbs count=$tcount $ddopt > /dev/null 2>&1
+hexdump -C "$tgt"|head -n 20
 echo "write:random"
 dd if=/dev/urandom of="$tgt" bs=$ddbs count=$tcount $ddopt > /dev/null 2>&1
+hexdump -C "$tgt"|head -n 20
 echo "write:0x00"
 dd if=/dev/zero of="$tgt" bs=$ddbs count=$tcount $ddopt > /dev/null 2>&1
+hexdump -C "$tgt"|head -n 20
+sync
 
 # check file or block
 echo ""
 test -b "$tgt"
-if [ $? != 0 ];then rm -i "$tgt";fi
+if [ $tgttype == 1 ];then
+    rm -i "$tgt"
+fi
+
 echo -e "\n---- done ----\n"
+
 exit 0
