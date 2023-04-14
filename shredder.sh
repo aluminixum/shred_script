bd1b72c1f5fc207dc7df5272c3f86fc99dce7227
aluminixum <aluminixum@gmail.com>
2023-04-14 15:40:51 +0900

    ???

--- shredder.sh
+++ shredder.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/bin/sh
 
 # 想定外のエラーが出た時点で終了するためのもの(最終手段)
 # -e で有効化
@@ -8,8 +8,16 @@ set -e
 
 # sudoのバージョン情報をnullに投げて処理
 # 追記:sudoがコアパッケージになかったのでコメントアウト
+#sudo -V > /dev/null 2>&1
+
+
 # root環境での実行する想定
-# sudo -V > /dev/null 2>&1
+# 'id -u'でuidが0でのみ実行、
+# それ以外の場合はエラーメッセージ出力(仮)
+if [ `id -u` -ne 0 ];then
+    echo "error"
+    exit 1
+fi
 
 
 # 区切り文字を改行のみにする（デフォルトはスペース、タブ、改行）
@@ -40,11 +48,13 @@ tgt="$1"
 # ディレクトリの場合は1で
 # 'find $tgt'で階層内のファイルを取得
 echo ""
-test -b "$tgt"
-if [ $? -eq 0 ]; then
-    tgt_type=0
-elif [ $? -eq 1]; then
-    tgt_type=1
+tge_type=`test -b "$tgt"`
+if [ $tgt_type -eq 0 ];then
+    #tgt_type=0
+    block_delete "$tgt"
+elif [ $tgt_type -eq 1 ];then
+    #tgt_type=1
+    file_delete "$tgt"
 else
     echo "### ERROR ###"
     exit 1
@@ -56,10 +66,40 @@ ddopt="oflag=direct,sync conv=notrunc,noerror,fsync status=progress"
 
 ### ここまで共通
 
-if [ $tgt_type -eq 0 ]; then
-    
+
+### ファイルとディレクトリの場合
+
+# ファイルかディレクトリかを判断
+
+
+# ファイル単位での消去は共通なので
+# ファイル位置かinode番号を引数で受け取る
+function file_delete(){
+    # ファイル位置パスを引数で受け取る
+    local tfile="$1"
+
+    # ファイルの格納されてるドライブを確認
+    tgt_dev=`df $tgt|grep "/dev"|cut -f 1 -d " "`
+
+    # デバイスのブロックサイズ(論理ではなく物理)を取得
+    ddbs=`cat /sys/block/$tgt_dev/queue/physical_block_size`
+}
+
 
 ### ここからファイル向け
+
+
+# ファイル向け関数
+function file_delete(){
+    # local変数としてtmode(FileTarGeT)に第1引数の$1を代入
+    local ftgt="$1"
+    # ファイルかフォルダか判断
+    f_or_d=`test -d $ftgt`
+    if [ $f_or_d -eq 0 ];then
+    # ファイルでもディレクトリでもforで回数指定する処理をここに書いたほうがいいかも
+
+}
+
 
 # 対象がファイルの場合は'df $tgt'
 # で格納されているドライブを確認して
@@ -69,14 +109,15 @@ tgt_dev=`df $tgt|grep "/dev"|cut -f 1 -d " "`
 
 # dd block size
 # fdiskなどで削除対象のブロックサイズを取得予定
-ddbs=`fdisk -l /dev/$tgt_dev|grep Sector|awk '{print $(NF-1)}'`
-# ddbs=512 <- old_version
+#ddbs=`fdisk -l /dev/$tgt_dev|grep Sector|awk '{print $(NF-1)}'`
+#ddbs=512 <- old_version
+ddbs=`cat /sys/block/$tgt_dev/queue/physical_block_size`
 
 
 # get file size (byte)
 # ファイルサイズは取得出来るのでブロックサイズでの余りを足せばいい？
 echo "get size..."
-tsize=`wc -c "$1"|awk '{print $1}'`
+tsize=`wc -c "$tgt"|awk '{print $1}'`
 echo "$tsize byte"
 
 
@@ -84,8 +125,8 @@ echo "$tsize byte"
 # file size / 512(block size) + 1block(512byte)
 # とりあえず512byteのドライブが多いからやっつけで
 echo "calc count..."
-tcount=`expr $tsize \/ $ddbs \+ 1`
-echo "$tcount count"
+ddcount=`expr $tsize \/ $ddbs \+ 1`
+echo "$ddcount count"
 
 
 # delete cache
@@ -100,40 +141,40 @@ echo -e "\n---- start ----\n"
 
 # Using /dev/urandom
 echo "write:random"
-dd if=/dev/urandom of="$tgt" bs=$ddbs count=$tcount $ddopt > /dev/null 2>&1
+dd if=/dev/urandom of="$tgt" bs=$ddbs count=$ddcount $ddopt > /dev/null 2>&1
 
 # Output 20 lines from the head
 hexdump -C "$tgt"|head -n 20
 
 # /dev/zero
 echo "write:0x00"
-dd if=/dev/zero of="$tgt" bs=$ddbs count=$tcount $ddopt > /dev/null 2>&1
+dd if=/dev/zero of="$tgt" bs=$ddbs count=$ddcount $ddopt > /dev/null 2>&1
 hexdump -C "$tgt"|head -n 20
 
 # パターンでの処理はそれぞれ関数化して番号を割り振って引数として受け取る
 # 000を273(oct)で置換して書き込み
 echo "write:0xBB"
-tr "\000" "\273" < /dev/zero|dd of="$tgt" bs=$ddbs count=$tcount $ddopt > /dev/null 2>&1
+tr "\000" "\273" < /dev/zero|dd of="$tgt" bs=$ddbs count=$ddcount $ddopt > /dev/null 2>&1
 hexdump -C "$tgt"|head -n 20
 
 echo "write:0x33"
-tr "\000" "\063" < /dev/zero|dd of="$tgt" bs=$ddbs count=$tcount $ddopt > /dev/null 2>&1
+tr "\000" "\063" < /dev/zero|dd of="$tgt" bs=$ddbs count=$ddcount $ddopt > /dev/null 2>&1
 hexdump -C "$tgt"|head -n 20
 
 echo "write:0xFF"
-tr "\000" "\377" < /dev/zero|dd of="$tgt" bs=$ddbs count=$tcount $ddopt > /dev/null 2>&1
+tr "\000" "\377" < /dev/zero|dd of="$tgt" bs=$ddbs count=$ddcount $ddopt > /dev/null 2>&1
 hexdump -C "$tgt"|head -n 20
 
 echo "write:0x77"
-tr "\000" "\167" < /dev/zero|dd of="$tgt" bs=$ddbs count=$tcount $ddopt > /dev/null 2>&1
+tr "\000" "\167" < /dev/zero|dd of="$tgt" bs=$ddbs count=$ddcount $ddopt > /dev/null 2>&1
 hexdump -C "$tgt"|head -n 20
 
 echo "write:random"
-dd if=/dev/urandom of="$tgt" bs=$ddbs count=$tcount $ddopt > /dev/null 2>&1
+dd if=/dev/urandom of="$tgt" bs=$ddbs count=$ddcount $ddopt > /dev/null 2>&1
 hexdump -C "$tgt"|head -n 20
 
 echo "write:0x00"
-dd if=/dev/zero of="$tgt" bs=$ddbs count=$tcount $ddopt > /dev/null 2>&1
+dd if=/dev/zero of="$tgt" bs=$ddbs count=$ddcount $ddopt > /dev/null 2>&1
 hexdump -C "$tgt"|head -n 20
 sync
 
